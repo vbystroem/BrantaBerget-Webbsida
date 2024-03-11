f1cc9b2f030dde930b398f4977c47f9ac3b4e3a6
ebengtssonn <96054751+ebengtssonn@users.noreply.github.com>
2024-03-11 19:23:42 +0100

    Darkmode-switch
    Har lagt till switchen i nav på alla(?) sidor och dess funktion i app.js och scss. (valde bara nån random bild till light på startsidan)

--- js/app.js
+++ js/app.js
@@ -38,3 +38,54 @@ let bookings = [];
     });
   }
 
+
+
+
+  // Darkmode-switch
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const elementsToToggle = [document.body, document.querySelector('.landing-image-2'), document.querySelector('.landing-image')];
+
+  // Lägg till lyssnare för klick på dark mode-toggle
+  darkModeToggle.addEventListener('change', () => toggleDarkMode(elementsToToggle));
+
+  // Kontrollera och uppdatera dark mode vid sidans laddning
+  checkDarkMode(darkModeToggle, elementsToToggle);
+
+  // Lägg till lyssnare för sidans avslutning (när användaren navigerar till en annan sida)
+  window.addEventListener('beforeunload', () => {
+    saveDarkModeState(darkModeToggle);
+  });
+});
+
+function toggleDarkMode(elements) {
+  const darkModeToggle = elements[0]; // Dark mode toggle is assumed to be the first element
+  const isDarkMode = darkModeToggle.checked;
+
+  elements.slice(1).forEach(element => {
+    element.classList.toggle('light-mode', isDarkMode);
+  });
+}
+
+function checkDarkMode(darkModeToggle, elements) {
+  const storedDarkModeState = getDarkModeState();
+
+  if (storedDarkModeState !== null) {
+    darkModeToggle.checked = storedDarkModeState;
+    elements.slice(1).forEach(element => {
+      element.classList.toggle('light-mode', storedDarkModeState);
+    });
+  }
+}
+
+function saveDarkModeState(darkModeToggle) {
+  const isDarkMode = darkModeToggle.checked;
+  localStorage.setItem('darkModeState', JSON.stringify(isDarkMode));
+}
+
+function getDarkModeState() {
+  const storedState = localStorage.getItem('darkModeState');
+  return storedState !== null ? JSON.parse(storedState) : null;
+}
+
