6d6ec7d55258e6306b92857128a28dbd07487ba7
vbystroem <veronica.bystroem@gmail.com>
2024-03-18 21:23:11 +0100

    bokningssida och darkmode

--- js/app.js
+++ js/app.js
@@ -21,71 +21,13 @@ let bookings = [];
 
       localStorage.setItem('Bokningar', JSON.stringify(bookings));
     };
-    const confirmBooking = () => {
-    let bookingconfirm = JSON.parse(window.localStorage.getItem('Bokningar'));
-    console.log(bookingconfirm);
-    }
-    function validateForm() {
-      switch(h) {
-        default:
-          document.querySelector("form").reset();
-      }
-        
+
     document.addEventListener('DOMContentLoaded', () => {
       document.getElementById("btn").addEventListener("click", addBooking);
-      document.getElementById("btn").addEventListener("click", confirmBooking);
-      document.getElementById("btn").addEventListener("click", validateForm);
     });
-  }
+    
+    
+  
 
 
-
-
-  // Darkmode-switch
-
-document.addEventListener('DOMContentLoaded', () => {
-  const darkModeToggle = document.getElementById('darkModeToggle');
-  const elementsToToggle = [document.body, document.querySelector('body'), document.querySelector('.bg-image'), document.querySelector('.intro-text'), document.querySelector('.intro-text-2'), document.querySelector('.intro-heading')];
-
-  // Lägg till lyssnare för klick på dark mode-toggle
-  darkModeToggle.addEventListener('change', () => toggleDarkMode(elementsToToggle));
-
-  // Kontrollera och uppdatera dark mode vid sidans laddning
-  checkDarkMode(darkModeToggle, elementsToToggle);
-
-  // Lägg till lyssnare för sidans avslutning (när användaren navigerar till en annan sida)
-  window.addEventListener('beforeunload', () => {
-    saveDarkModeState(darkModeToggle);
-  });
-});
-
-function toggleDarkMode(elements) {
-  const darkModeToggle = elements[0]; // Dark mode toggle is assumed to be the first element
-  const isDarkMode = darkModeToggle.checked;
-
-  elements.slice(1).forEach(element => {
-    element.classList.toggle('light-mode', isDarkMode);
-  });
-}
-
-function checkDarkMode(darkModeToggle, elements) {
-  const storedDarkModeState = getDarkModeState();
-
-  if (storedDarkModeState !== null) {
-    darkModeToggle.checked = storedDarkModeState;
-    elements.slice(1).forEach(element => {
-      element.classList.toggle('light-mode', storedDarkModeState);
-    });
-  }
-}
-
-function saveDarkModeState(darkModeToggle) {
-  const isDarkMode = darkModeToggle.checked;
-  localStorage.setItem('darkModeState', JSON.stringify(isDarkMode));
-}
-
-function getDarkModeState() {
-  const storedState = localStorage.getItem('darkModeState');
-  return storedState !== null ? JSON.parse(storedState) : null;
-}
 
